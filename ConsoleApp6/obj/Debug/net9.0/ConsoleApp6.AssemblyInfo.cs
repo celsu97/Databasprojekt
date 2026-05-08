3857562a34f75d6797f76a4981d3fbbaf12ee480
Celina <celsu124@gmail.com>
2026-05-08 14:51:20 +0200

    Changed SDK from NET9.0 to .NET8.0

--- ConsoleApp6/obj/Debug/net9.0/ConsoleApp6.AssemblyInfo.cs
+++ ConsoleApp6/obj/Debug/net9.0/ConsoleApp6.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+337eeae46e3cc268bb30defe1b90dc7c75778463")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
